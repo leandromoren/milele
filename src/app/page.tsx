4e1017b046348733100389298acdacbeb7429e98
Leandro Moren <leandro.moren@outlook.com>
2025-03-30 15:25:06 -0300

    Agrego variable de entorno para el backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,7 @@ export default function Home() {
   const [productos, setProductos] = useState<Producto[]>([]);
 
   useEffect(() => {
-    console.log(
-      `URL completa: ${process.env.NEXT_PUBLIC_BACKEND_URL}/productos`
-    );
-    fetch(`http://127.0.0.1:8000/productos`) // Asegúrate de usar el puerto correcto
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/`) // Asegúrate de usar el puerto correcto
       .then((res) => {
         if (!res.ok) {
           throw new Error(`HTTP error! Status: ${res.status}`);
@@ -26,7 +23,7 @@ export default function Home() {
       .then((data) => {
         console.log("📦 Respuesta del backend:", data);
         if (data && data.data) {
-          setProductos(data.data);
+          setProductos(data);
         } else if (Array.isArray(data)) {
           setProductos(data); // Si data es directamente el array
         } else {
@@ -40,7 +37,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex justify-center items-center p-4">
       <div className="w-full max-w-4xl bg-white p-6 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold text-center mb-4">
+        <h1 className="text-2xl font-bold text-center mb-4 text-red-800">
           Lista de Productos
         </h1>
         <table className="w-full border-collapse border border-gray-300">
@@ -57,7 +54,11 @@ export default function Home() {
             {productos && productos.length > 0 ? (
               productos.map((producto) => (
                 <tr key={producto.id} className="text-center bg-green-900">
-                  {}
+                  <td className="border p-2">{producto.id}</td>
+                  <td className="border p-2">{producto.titulo}</td>
+                  <td className="border p-2">{producto.precio}</td>
+                  <td className="border p-2">{producto.descripcion}</td>
+                  <td className="border p-2">{producto.valoracion}</td>
                 </tr>
               ))
             ) : (
